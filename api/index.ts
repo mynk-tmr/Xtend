f0e6632bcaf2896cc0b36521cf72c857aef797c1
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-03-16 02:48:34 +0530

    deploy: attempt 2

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-app.get("/", (_req: Request, res: Response) => {
+app.get("/api/", (_req: Request, res: Response) => {
   return res.send("Backend!");
 });
 
