d6c64d1e1caf5bacab4d9c09e2aec8401260401d
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-03-18 20:33:31 +0530

    add 404 page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,9 +1,14 @@
 import { authRoutes } from "@/features/auth";
 import { homepageRoutes } from "@/features/home";
+import { notFoundRoutes } from "./404";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const Router = () => {
-  const router = createBrowserRouter([authRoutes, homepageRoutes]);
+  const router = createBrowserRouter([
+    authRoutes,
+    homepageRoutes,
+    notFoundRoutes,
+  ]);
   return <RouterProvider router={router} />;
 };
 
